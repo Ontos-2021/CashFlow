14f7136c61f4695f29d280a0df7547fdf8502a73
_Ontos_ <josemercado.musica@gmail.com>
2021-07-22 17:31:30 -0300

    Primer bosquejo de lo que sería el juego.

---
+++ clases.py
@@ -0,0 +1,33 @@
+class Billetera:
+    def __init__(self, usuario, saldo_inicial=0):
+        self.dueño = usuario
+        self.saldo = saldo_inicial
+
+    def __str__(self):
+        return f"Soy una billetera con ${self.saldo} de saldo"
+
+    def ingresar_dinero(self, monto):
+        self.saldo += monto
+        print(f"Se ha ingresado ${monto} a la billetera de {self.dueño}")
+
+    def gastar_dinero(self, monto):
+        self.saldo -= monto
+
+    def vaciar_billetera(self):
+        self.saldo = 0
+
+
+class Usuario:
+    def __init__(self, nombre, profesion):
+        self.nombre = nombre
+        self.profesion = profesion
+        self.billetera = Billetera(self, profesion.saldo_inicial)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Profesion:
+    def __init__(self, nombre, saldo_inicial):
+        self.nombre = nombre
+        self.saldo_inicial = saldo_inicial
---
+++ main.py
@@ -0,0 +1,22 @@
+from clases import *
+
+
+def main():
+    # Creo una profesión
+    ing = Profesion("Ingeniero Civil Industrial", 1000)
+
+    # Creo un usuario
+    usuario1 = Usuario("José Miguel Mercado Gutiérrez", ing)
+
+    # Imprimo el saldo del usuario
+
+    print(f"El saldo del usuario es: ${usuario1.billetera.saldo}")
+
+    # Le agrego $1.000 pesos más
+    usuario1.billetera.ingresar_dinero(1000)
+
+    print(f"El saldo del usuario es: ${usuario1.billetera.saldo}")
+
+
+if __name__ == "__main__":
+    main()
